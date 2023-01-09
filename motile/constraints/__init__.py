5ee69655db130e226716b0fdcc198ebb71940c8b
Jan Funke <funkej@janelia.hhmi.org>
2023-01-09 15:50:14 -0500

    Add MaxChildren constraint

--- motile/constraints/__init__.py
+++ motile/constraints/__init__.py
@@ -1,3 +1,4 @@
 from .constraint import Constraint  # noqa
+from .max_children import MaxChildren  # noqa
 from .max_parents import MaxParents  # noqa
 from .select_edge_nodes import SelectEdgeNodes  # noqa
---
+++ motile/constraints/max_children.py
@@ -0,0 +1,43 @@
+from ..variables import EdgeSelected
+from .constraint import Constraint
+import pylp
+
+
+class MaxChildren(Constraint):
+    """Ensures that every selected node has no more than ``max_children``
+    selected edges to the next frame.
+
+    Adds the following linear constraint for each node::
+
+      sum(next) <= max_children
+
+    Args:
+
+        max_children (int):
+            The maximum number of children allowed.
+    """
+
+    def __init__(self, max_children):
+
+        self.max_children = max_children
+
+    def instantiate(self, solver):
+
+        edge_indicators = solver.get_variables(EdgeSelected)
+
+        constraints = []
+        for node in solver.graph.nodes:
+
+            constraint = pylp.LinearConstraint()
+
+            # all incoming edges
+            for edge in solver.graph.next_edges(node):
+                constraint.set_coefficient(edge_indicators[edge].index, 1)
+
+            # relation, value
+            constraint.set_relation(pylp.Relation.LessEqual)
+
+            constraint.set_value(self.max_children)
+            constraints.append(constraint)
+
+        return constraints
