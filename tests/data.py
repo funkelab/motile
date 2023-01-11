f27cbe3a5e8e5d5e58dc719b0f29807a1e8eac24
Jan Funke <funkej@janelia.hhmi.org>
2023-01-11 13:41:44 -0500

    Add high-level API test

---
+++ tests/data.py
@@ -0,0 +1,50 @@
+import motile
+
+
+def create_arlo_graph():
+    '''Create the "Arlo graph", a simple toy graph for testing:
+
+       x
+       |
+    200|           6
+       |         /
+    150|   1---3---5
+       |     x   x
+    100|   0---2---4
+        ------------------------------------ t
+           0   1   2
+    '''
+
+    cells = [
+            {'id': 0, 't': 0, 'x': 101, 'score': 1.0},
+            {'id': 1, 't': 0, 'x': 150, 'score': 1.0},
+            {'id': 2, 't': 1, 'x': 100, 'score': 1.0},
+            {'id': 3, 't': 1, 'x': 151, 'score': 1.0},
+            {'id': 4, 't': 2, 'x': 102, 'score': 1.0},
+            {'id': 5, 't': 2, 'x': 149, 'score': 1.0},
+            {'id': 6, 't': 2, 'x': 200, 'score': 1.0}
+    ]
+
+    edges = [
+        {'source': 0, 'target': 2, 'prediction_distance': 1.0},
+        {'source': 1, 'target': 3, 'prediction_distance': 1.0},
+        {'source': 0, 'target': 3, 'prediction_distance': 50.0},
+        {'source': 1, 'target': 2, 'prediction_distance': 50.0},
+        {'source': 2, 'target': 4, 'prediction_distance': 2.0},
+        {'source': 3, 'target': 5, 'prediction_distance': 2.0},
+        {'source': 2, 'target': 5, 'prediction_distance': 49.0},
+        {'source': 3, 'target': 4, 'prediction_distance': 49.0},
+        {'source': 3, 'target': 6, 'prediction_distance': 3.0}
+    ]
+
+    graph = motile.TrackGraph()
+    graph.add_nodes_from([
+        (cell['id'], cell)
+        for cell in cells
+    ])
+    graph.add_edges_from([
+        (edge['source'], edge['target'], edge)
+        for edge in edges
+    ])
+
+    return graph
---
+++ tests/test_api.py
@@ -0,0 +1,32 @@
+from data import create_arlo_graph
+from motile.constraints import MaxParents, MaxChildren
+from motile.costs import NodeSelection, EdgeSelection, Appear, Split
+from motile.variables import NodeSelected, EdgeSelected, NodeSplit, NodeAppear
+import motile
+import unittest
+
+
+class TestAPI(unittest.TestCase):
+
+    def test_solver(self):
+
+        graph = create_arlo_graph()
+
+        solver = motile.Solver(graph)
+        solver.add_costs(
+            NodeSelection(
+                weight=-1.0,
+                attribute='score',
+                constant=-100.0))
+        solver.add_costs(
+            EdgeSelection(
+                weight=1.0,
+                attribute='prediction_distance'))
+        solver.add_costs(Appear(constant=200.0))
+        solver.add_costs(Split(constant=100.0))
+        solver.add_constraints(MaxParents(1))
+        solver.add_constraints(MaxChildren(2))
+
+        solution = solver.solve()
+
+        assert solution.get_value() == -198
