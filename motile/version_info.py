4eb733b718a0a05f1e490044d80b288b801f3f2b
Jan Funke <funkej@janelia.hhmi.org>
2023-01-31 12:02:41 -0500

    Add version_info.py

---
+++ motile/version_info.py
@@ -0,0 +1,30 @@
+__major__   = 0
+__minor__   = 1
+__patch__   = 1
+__tag__     = ''
+__version__ = '{}.{}.{}{}'.format(__major__, __minor__, __patch__, __tag__).strip('.')
+
+class _Version(object):
+
+    def major(self):
+        return __major__
+
+    def minor(self):
+        return __minor__
+
+    def patch(self):
+        return __patch__
+
+    def tag(self):
+        return __tag__
+
+    def version(self):
+        return __version__
+
+    def __str__(self):
+        return self.version()
+
+_version = _Version()
+
+if __name__ == "__main__":
+    print(_version)
