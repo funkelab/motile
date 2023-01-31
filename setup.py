af359fa9cede0b0d6059b0d2f301dbbfe08bddaa
Jan Funke <funkej@janelia.hhmi.org>
2023-01-31 11:59:47 -0500

    Use version_info.py in setup.py

--- setup.py
+++ setup.py
@@ -1,8 +1,15 @@
 from setuptools import setup
 
+name = 'motile'
+here = os.path.abspath(os.path.dirname(__file__))
+version_info = {}
+with open(os.path.join(here, name, 'version_info.py')) as fp:
+    exec(fp.read(), version_info)
+version = version_info['_version']
+
 setup(
-        name='motile',
-        version='0.1.1',
+        name=name,
+        version=str(version),
         description='Multi-Object Tracker using Integer Linear Equations',
         url='https://github.com/funkelab/motile',
         author='Jan Funke',
