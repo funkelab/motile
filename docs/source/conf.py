8aaea403c79939b5914248ffa9e5d8426186dc99
Jan Funke <funkej@janelia.hhmi.org>
2023-02-28 17:20:36 -0500

    Rebuild HTML documentation

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -42,9 +42,6 @@ exclude_patterns = []
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
 html_theme = 'sphinx_rtd_theme'
-html_theme_options = {
-    'logo_only': True
-}
 html_logo = 'motile.svg'
 html_static_path = ['_static']
 html_css_files = [
