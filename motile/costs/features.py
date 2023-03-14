ac2d4044b3714bfb482f58516f947d60f665a5d4
Benjamin Gallusser <bgallusser@googlemail.com>
2023-03-14 09:35:59 -0400

    Fix bug: Feature matrix resizing (#13)
    * Fix bug: Feature matrix resizing
    
    When adding unseen variables/features, the feature matrix gets
    efficiently resized to accommodate the new entry with
    `numpy.ndarray.resize`. However, this command can possibly shrink
     an array, which is never desired here. We now ensure that the
    feature matrix will grow or keep the its size in both dimensions at each resize
    operation.
    
    * Fix bug: Adding new features
    
    While adding new rows to a numpy array works in place, adding new
    columns requires copying the array. The current solution adds separate
    methods for resizing the two dimensions of the feature matrix.
    
    * Clean up feature resize fix
    
    * Add edge variables to SSVM ground-truth
    
    * Update structsvm test
    
    ---------
    
    Co-authored-by: Jan Funke <funkej@janelia.hhmi.org>

--- motile/costs/features.py
+++ motile/costs/features.py
@@ -15,7 +15,23 @@ class Features:
         if num_features is None:
             num_features = self._values.shape[1]
 
-        self._values.resize((num_variables, num_features), refcheck=False)
+        # Resize features first for efficiency
+        if num_features > self._values.shape[1]:
+            self._increase_features(num_features)
+
+        if num_variables > self._values.shape[0]:
+            self._increase_variables(num_variables)
+
+    def _increase_variables(self, num_variables: int) -> None:
+        # Increasing size without copying works only in dim 0
+        self._values.resize(
+            (num_variables, self._values.shape[1]), refcheck=False)
+
+    def _increase_features(self, num_features: int) -> None:
+        # Need to copy the array when increasing size of dim 1
+        shape = (self._values.shape[0], num_features - self._values.shape[1])
+        new_features = np.zeros(shape, dtype=self._values.dtype)
+        self._values = np.hstack((self._values, new_features))
 
     def add_feature(
         self, variable_index: int, feature_index: int, value: float
@@ -23,7 +39,9 @@ class Features:
         num_variables, num_features = self._values.shape
 
         if variable_index >= num_variables or feature_index >= num_features:
-            self.resize(variable_index + 1, feature_index + 1)
+            self.resize(
+                max(variable_index + 1, num_variables),
+                max(feature_index + 1, num_features))
 
         self._values[variable_index, feature_index] += value
 
