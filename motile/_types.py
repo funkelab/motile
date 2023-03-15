51d9d37e4f9ac21da37878104a8735683b67f583
Talley Lambert <talley.lambert@gmail.com>
2023-03-15 16:16:44 -0400

    add comment

--- motile/_types.py
+++ motile/_types.py
@@ -2,10 +2,10 @@ from __future__ import annotations
 
 from typing import TypeAlias
 
-# Nodes are represented as integers.
+# Nodes are represented as integers, or a "meta-node" tuple of integers.
 NodeId: TypeAlias = "int | tuple[int, ...]"
 
-# Edges are represented as tuples of node IDs.
+# Edges are represented as tuples of NodeId.
 # (0, 1) is an edge from node 0 to node 1.
 # ((0, 1), 2) is a hyperedge from nodes 0 and 1 to node 2 (i.e. a merge).
 # ((0,), (1, 2)) is a hyperedge from node 0 to nodes 1 and 2 (i.e. a split).
