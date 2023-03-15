29f33e3238bd5b7f00bad1c540e3bd9f583584af
Florian Jug <florian.jug@fht.org>
2023-03-15 12:13:27 -0400

    Hyperedges enabled and api according to wishlist

--- motile/track_graph.py
+++ motile/track_graph.py
@@ -8,6 +8,8 @@ logger = logging.getLogger(__name__)
 if TYPE_CHECKING:
     from networkx.classes import DiGraph
 
+EdgeTuple = "tuple[int|tuple[int,...],...]"
+
 
 class TrackGraph:
     """A :class:`networkx.DiGraph` of objects with positions in time and space,
@@ -21,7 +23,7 @@ class TrackGraph:
         graph (:class:`networkx.DiGraph`):
 
             Optional graph data to pass to the :class:`networkx.DiGraph`
-            constructor as ``incoming_graph_data``. This can be used to
+            constructor as ``graph_data``. This can be used to
             populate a track graph with entries from a generic
             ``networkx`` graph. If None (default) an empty
             graph is created.  The data can be an edge list, or any
@@ -33,24 +35,44 @@ class TrackGraph:
 
             The name of the node attribute that corresponds to the frame (i.e.,
             the time dimension) of the object. Defaults to ``'t'``.
+
+        has_hyperedges (``boolean``, optional):
+
+            Indicates if the given ``graph_data`` contains nodes that indicate
+            hyper edges. In the given ``networkx`` graph a hyperedge is represented
+            by a node that does not have the ``frame_attribute`` and is itself
+            connected to all nodes that should form a hyper edge.
     """
 
     def __init__(
         self,
-        graph: DiGraph,
+        nx_graph: DiGraph = None,
         frame_attribute: str = "t",
-        is_bipartite: bool = False,
     ):
         self.frame_attribute = frame_attribute
-        self.is_bipartite = is_bipartite
         self._graph_changed = True
 
-        self.nx_graph = graph
+        self.nodes: dict[Hashable, dict[Hashable, Any]] = {}
+        self.edges: dict[EdgeTuple, dict[Hashable, Any]] = {}
+        self.prev_edges: dict[Hashable, list[Hashable]] = {}
+        self.next_edges: dict[Hashable, list[Hashable]] = {}
 
-        self.nodes: dict[Hashable, Any] = {
-            node: graph.nodes[node]
-            for node in graph.nodes
-            if frame_attribute in graph.nodes[node]
+        if nx_graph:
+            self.add_from_nx_graph(nx_graph)
+
+        self._update_metadata()
+
+    def add_node(self, node_id: Hashable, data: dict[Hashable, Any]):
+        self.nodes[node_id] = data
+
+    def add_edge(self, edge_tuple: EdgeTuple, data: dict[Hashable, Any]):
+        self.edges[edge_tuple] = data
+
+    def add_from_nx_graph(self, nx_graph):
+        self.nodes = {
+            node: nx_graph.nodes[node]
+            for node in nx_graph.nodes
+            if self.frame_attribute in nx_graph.nodes[node]
         }
 
         self.prev_edges: dict[Hashable, list[Hashable]] = {
@@ -60,42 +82,57 @@ class TrackGraph:
             node: [] for node in self.nodes
         }
 
-        if is_bipartite:
-            self.edges = {
-                self._assignment_node_to_edge_tuple(
-                    graph, assignment_node
-                ): graph.nodes[assignment_node]
-                for assignment_node in graph.nodes
-                if frame_attribute not in graph.nodes[assignment_node]
-            }
-        else:
-            self.edges = graph.edges
-            for u, v in graph.edges:
+        for (u, v), data in nx_graph.edges.items():
+            # do nothing when you have an nx_edge leaving a hyperedge node
+            if self._is_hyperedge_nx_node(nx_graph, u):
+                continue
+            # add hyperedge when nx_edge leads to hyperedge node
+            if self._is_hyperedge_nx_node(nx_graph, v):
+                (
+                    edge,
+                    in_nodes,
+                    out_nodes,
+                ) = self._hyperedge_nx_node_to_edge_tuple_and_neighbors(nx_graph, v)
+                if (
+                    edge not in self.edges
+                ):  # avoid adding hyperedges having multiple source nodes
+                    self.edges[edge] = data
+                    for in_node in in_nodes:
+                        self.next_edges[in_node].append(edge)
+                    for out_node in out_nodes:
+                        self.prev_edges[out_node].append(edge)
+            else:  # add a regular edge otherwise
+                self.edges[(u, v)] = data
                 self.prev_edges[v].append((u, v))
                 self.next_edges[u].append((u, v))
 
-        self._update_metadata()
+    def _is_hyperedge_nx_node(self, nx_graph: Any, nx_node: Any) -> bool:
+        return self.frame_attribute not in nx_graph.nodes[nx_node]
 
-    def _assignment_node_to_edge_tuple(
-        self, graph: DiGraph, assignment_node: Hashable
-    ) -> tuple[tuple[Hashable, ...], ...]:
-        in_nodes = graph.predecessors(assignment_node)
-        out_nodes = graph.successors(assignment_node)
-        nodes = in_nodes + out_nodes
+    def _hyperedge_nx_node_to_edge_tuple_and_neighbors(
+        self, nx_graph: DiGraph, hyperedge_node: Any
+    ) -> tuple[Hashable, ...]:
+        assert self._is_hyperedge_nx_node(nx_graph, hyperedge_node)
 
-        frames = sorted(node[self.frame_attribute] for node in nodes)
+        in_nodes = list(nx_graph.predecessors(hyperedge_node))
+        out_nodes = list(nx_graph.successors(hyperedge_node))
+        nx_nodes = in_nodes + out_nodes
+
+        frameset = set(
+            nx_graph.nodes[nx_node][self.frame_attribute] for nx_node in nx_nodes
+        )
+        frames = list(sorted(frameset))
 
         edge_tuple = tuple(
-            tuple(node for node in nodes if node[self.frame_attribute] == frame)
+            tuple(
+                node
+                for node in nx_nodes
+                if nx_graph.nodes[node][self.frame_attribute] == frame
+            )
             for frame in frames
         )
 
-        for in_node in in_nodes:
-            self.next_edges[in_nodes].append(edge_tuple)
-        for out_node in out_nodes:
-            self.prev_edges[out_nodes].append(edge_tuple)
-
-        return edge_tuple
+        return edge_tuple, in_nodes, out_nodes
 
     def get_frames(self) -> tuple[int | None, int | None]:
         """Get a tuple ``(t_begin, t_end)`` of the first and last frame
@@ -137,14 +174,5 @@ class TrackGraph:
         frames = self._nodes_by_frame.keys()
         self.t_begin = min(frames)
         self.t_end = max(frames) + 1
-        # ensure edges point forwards in time
-        if not self.is_bipartite:
-            for u, v in self.edges:
-                t_u = self.nodes[u][self.frame_attribute]
-                t_v = self.nodes[v][self.frame_attribute]
-                assert t_u < t_v, (
-                    f"Edge ({u}, {v}) does not point forwards in time, but "
-                    f"from frame {t_u} to {t_v}"
-                )
 
         self._graph_changed = False
