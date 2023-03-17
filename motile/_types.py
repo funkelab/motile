bba6bbda4292f3114660a42970c319cdcb369fa8
Talley Lambert <talley.lambert@gmail.com>
2023-03-17 14:44:24 -0400

    add graph object

--- motile/_types.py
+++ motile/_types.py
@@ -1,9 +1,13 @@
 from __future__ import annotations
 
-from typing import TypeAlias
+from typing import Any, Hashable, TypeAlias, Union
 
 # Nodes are represented as integers, or a "meta-node" tuple of integers.
-NodeId: TypeAlias = "int | tuple[int, ...]"
+NodeId: TypeAlias = Union[int, tuple[int, ...]]
+
+# objects in the graph are represented as dicts
+# eg. { "id": 1, "x": 0.5, "y": 0.5, "t": 0 }
+GraphObject: TypeAlias = dict[Hashable, Any]
 
 # Edges are represented as tuples of NodeId.
 # (0, 1) is an edge from node 0 to node 1.
