a2d7b9bd151053f3d6200178141444b825d1773c
Jan Funke <funkej@janelia.hhmi.org>
2024-01-22 11:29:47 -0500

    Add ignore attribute to appear/disappear costs

--- motile/costs/appear.py
+++ motile/costs/appear.py
@@ -23,19 +23,33 @@ class Appear(Costs):
 
         constant:
             A constant cost for each node that starts a track.
+
+        ignore_attribute:
+            The name of an optional node attribute that, if it is set and
+            evaluates to ``True``, will not set the appear costs for that node.
+            This is useful to allow nodes in the first frame to appear at no
+            cost.
     """
 
     def __init__(
-        self, weight: float = 1, attribute: str | None = None, constant: float = 0
+        self,
+        weight: float = 1,
+        attribute: str | None = None,
+        constant: float = 0,
+        ignore_attribute: str | None = None,
     ) -> None:
         self.weight = Weight(weight)
         self.constant = Weight(constant)
         self.attribute = attribute
+        self.ignore_attribute = ignore_attribute
 
     def apply(self, solver: Solver) -> None:
         appear_indicators = solver.get_variables(NodeAppear)
 
         for node, index in appear_indicators.items():
+            if self.ignore_attribute is not None:
+                if solver.graph.nodes[node].get(self.ignore_attribute, False):
+                    continue
             if self.attribute is not None:
                 solver.add_variable_cost(
                     index, solver.graph.nodes[node][self.attribute], self.weight
--- motile/costs/disappear.py
+++ motile/costs/disappear.py
@@ -16,13 +16,23 @@ class Disappear(Costs):
     Args:
         constant (float):
             A constant cost for each node that ends a track.
+
+        ignore_attribute:
+            The name of an optional node attribute that, if it is set and
+            evaluates to ``True``, will not set the disappear costs for that
+            node. This is useful to allow nodes in the last frame to disappear
+            at no cost.
     """
 
     def __init__(self, constant: float) -> None:
         self.constant = Weight(constant)
+        self.ignore_attribute = ignore_attribute
 
     def apply(self, solver: Solver) -> None:
         disappear_indicators = solver.get_variables(NodeDisappear)
 
         for index in disappear_indicators.values():
+            if self.ignore_attribute is not None:
+                if solver.graph.nodes[node].get(self.ignore_attribute, False):
+                    continue
             solver.add_variable_cost(index, 1.0, self.constant)
