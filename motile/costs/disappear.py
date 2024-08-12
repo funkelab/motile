e836b5788401a32e738aa8425b48eb44a7a19a21
Benjamin Gallusser <bgallusser@googlemail.com>
2024-08-12 18:10:41 +0200

    Remove dis/appear cost in last/first frames

--- motile/costs/disappear.py
+++ motile/costs/disappear.py
@@ -13,6 +13,8 @@ if TYPE_CHECKING:
 class Disappear(Cost):
     """Cost for :class:`motile.variables.NodeDisappear` variables.
 
+    This is cost is not applied to nodes in the last frame of the graph.
+
     Args:
         constant (float):
             A constant cost for each node that ends a track.
@@ -20,8 +22,7 @@ class Disappear(Cost):
         ignore_attribute:
             The name of an optional node attribute that, if it is set and
             evaluates to ``True``, will not set the disappear cost for that
-            node. This is useful to allow nodes in the last frame to disappear
-            at no cost.
+            node.
     """
 
     def __init__(self, constant: float, ignore_attribute: str | None = None) -> None:
@@ -30,9 +31,12 @@ class Disappear(Cost):
 
     def apply(self, solver: Solver) -> None:
         disappear_indicators = solver.get_variables(NodeDisappear)
+        G = solver.graph
 
         for node, index in disappear_indicators.items():
             if self.ignore_attribute is not None:
-                if solver.graph.nodes[node].get(self.ignore_attribute, False):
+                if G.nodes[node].get(self.ignore_attribute, False):
                     continue
+            if G.nodes[node][G.frame_attribute] == G.get_frames()[1]:
+                continue
             solver.add_variable_cost(index, 1.0, self.constant)
